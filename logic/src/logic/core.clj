bdd3d9ee4bacda9fe051959332a37a841568735d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-05-14 20:42:01 +0200

    Solve Pb #2 & add test.

--- logic/src/logic/core.clj
+++ logic/src/logic/core.clj
@@ -2,12 +2,24 @@
   (:refer-clojure :exclude [==])
   (:use clojure.core.logic))
 
-;;P01
+;; P01
 ;; Find the last element of a list
 (defn lasto [last list]
-  (matche [list])
-  ([[]] fail)
-  ([[last]] succeed)
-  ([[_ . ?rest]] lasto last ?rest)
-  
-  ;;(([])))
+  (matche [list]
+  ([[]]          fail)
+  ([[last]]      succeed)
+  ([[_ . ?rest]] (lasto last ?rest))))
+
+(run* [q] (lasto q '(1 2 3 6)))
+
+;; P02
+;; Find the last but one element of a list.
+(defn lastbutoneo [p list]
+  (matche [list]
+          ([[]]   fail)
+          ([[_]] fail)
+          ([[p _]] succeed)
+          ([[_ . ?rest]] (lastbutoneo p ?rest))))
+
+(run* [q] (lastbutoneo q '(1 2 9 4 6)))
+ 
--- logic/test/logic/core_test.clj
+++ logic/test/logic/core_test.clj
@@ -1,7 +1,28 @@
 (ns logic.core-test
+  (:use logic.core)
   (:require [clojure.test :refer :all]
-            [logic.core :refer :all]))
+            [clojure.core.logic :refer [run* run fresh]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-lasto
+  (testing "fails for empty list"
+    (is (empty? (run* [q] (lasto q [])))))
+  (testing "gets last"
+    (is (= [3] (run* [q] (lasto q [1 2 3]))))))
+
+(deftest test-lastbutoneo
+  (testing "fails for empty list"
+    (is (empty? (run* [q] (lastbutoneo q [])))))
+  (testing "fails for a one element list"
+    (is (empty? (run* [q] (lastbutoneo q [2])))))
+  (testing "gets before last for a list of length"
+    (is (= [1] (run* [q] (lastbutoneo q [1 2])))))
+  (testing "gets before last for a list of length > 2"
+    (is (= [2] (run* [q] (lastbutoneo q [1 2 3]))))))
+
+
+
+
+
+
+
+
